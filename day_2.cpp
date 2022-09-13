1c63af641992283c790b5534166a433511221586
Umme Ara Mahinur Istia <102405573+istiaa@users.noreply.github.com>
2022-09-14 01:06:19 +0600

    Add files via upload

---
+++ day_2.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+#include<conio.h>
+using namespace std ;
+int main ()
+{
+  cout<<"Enter the weight of watermelon:" <<endl ;
+   int w ;        //w=watermelon
+  cin>>w ;
+  if (w%2==0)
+  {
+    cout << "yes" <<endl ;
+  }
+  else
+    {
+    cout << "no" <<endl ;
+  }
+    getch();
+}
