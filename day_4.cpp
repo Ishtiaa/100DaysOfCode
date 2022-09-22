f803f21518041bdbbf8348b7521da942a38aea14
Umme Ara Mahinur Istia <102405573+istiaa@users.noreply.github.com>
2022-09-22 13:37:08 +0600

    Add files via upload

---
+++ day_4.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+#include<conio.h>
+using namespace std ;
+
+int main ()
+{
+   char ch ;
+   cout<<"Enter any letter:" <<endl ;
+   cin>> ch ;
+   if (ch == 'A' || ch == 'E' || ch == 'I' || ch == 'O' || ch == 'U') {
+    cout << "vowel" ;
+    } else {
+    cout << "consonant" ;
+    }
+
+   getch() ;
+}
